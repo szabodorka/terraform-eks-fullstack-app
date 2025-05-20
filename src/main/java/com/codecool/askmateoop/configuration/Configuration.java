4f3c1e80eb7640e462083b073d4d2580fea181b6
bramlak <kalmarbalint97@gmail.com>
2025-05-20 11:49:06 +0200

    add answersDAO bean

--- src/main/java/com/codecool/askmateoop/configuration/Configuration.java
+++ src/main/java/com/codecool/askmateoop/configuration/Configuration.java
@@ -1,5 +1,7 @@
 package com.codecool.askmateoop.configuration;
 
+import com.codecool.askmateoop.dao.AnswersDAO;
+import com.codecool.askmateoop.dao.AnswersDaoJdbc;
 import com.codecool.askmateoop.dao.QuestionsDAO;
 import com.codecool.askmateoop.dao.QuestionsDaoJdbc;
 import org.springframework.boot.SpringBootConfiguration;
@@ -11,6 +13,11 @@ public class Configuration {
 //    TODO: Add the url of your database to the Environment Variables of the Run Configuration
 //    @Value("${askmate.database.url}")
 //    private String databaseUrl;
+
+    @Bean
+    public AnswersDAO answersDAO() {
+        return new AnswersDaoJdbc();
+    }
 
     @Bean
     public QuestionsDAO questionsDAO() {
