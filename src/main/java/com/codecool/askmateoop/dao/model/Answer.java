ee87e0e8d7d07239cd0a81f61bee07e59b8ed04d
bramlak <kalmarbalint97@gmail.com>
2025-05-21 11:09:39 +0200

    modify answer model class and add converter method

--- src/main/java/com/codecool/askmateoop/dao/model/Answer.java
+++ src/main/java/com/codecool/askmateoop/dao/model/Answer.java
@@ -1,6 +1,18 @@
 package com.codecool.askmateoop.dao.model;
 
+import com.codecool.askmateoop.controller.dto.answer.NewAnswerDTO;
+
 import java.time.LocalDateTime;
 
-public record Answer(int id, String title, String description, LocalDateTime created) {
+public record Answer(int id, String title, String message,int userId, LocalDateTime postDate, int questionId) {
+    public static Answer fromNewAnswerDTO(NewAnswerDTO newAnswerDTO){
+        return new Answer(
+                -1,
+                newAnswerDTO.title(),
+                newAnswerDTO.message(),
+                newAnswerDTO.userId(),
+                null,
+                newAnswerDTO.questionId()
+        );
+    }
 }
