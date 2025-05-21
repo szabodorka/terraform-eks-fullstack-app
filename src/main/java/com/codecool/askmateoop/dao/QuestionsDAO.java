91f2898ec6ee6d77403764de5fc7eedd520e687a
d0rk-a <szabodorkam@gmail.com>
2025-05-21 10:23:50 +0200

    updated QuestionsDAO signatures

--- src/main/java/com/codecool/askmateoop/dao/QuestionsDAO.java
+++ src/main/java/com/codecool/askmateoop/dao/QuestionsDAO.java
@@ -7,6 +7,6 @@ import java.util.List;
 public interface QuestionsDAO {
     List<Question> getAllQuestions();
     Question getQuestion(int id);
-    void createQuestion(Question question);
+    Question createQuestion(Question question);
     void deleteQuestion(int id);
 }
