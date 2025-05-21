65c8e2ae758dfaceef15126e985ae6ca449f1e28
d0rk-a <szabodorkam@gmail.com>
2025-05-21 14:41:48 +0200

    added Questions list and single Question component, Loading.jsx and css and post Question request for testing purposes

--- frontend/askmate-frontend/src/main.jsx
+++ frontend/askmate-frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './main.css'
+import Questions from "./pages/Questions/Questions.jsx";
 
 const router = createBrowserRouter([
     {
