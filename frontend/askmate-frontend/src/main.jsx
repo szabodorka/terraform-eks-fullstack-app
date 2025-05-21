efff87ce7e24a4b33ffffb0b291d30ae9efde12d
Gecse David <dadi.gecse@gmail.com>
2025-05-21 11:29:34 +0200

    setup react frontend

---
+++ frontend/askmate-frontend/src/main.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './main.css'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/register",
+        element: <Registration />,
+    },
+    {
+        path: "/u",
+        element: <Menu />,
+        children: [
+            {
+                path: "/u/questions",
+                element: <Questions />,
+            },
+            {
+                path: "/u/search/:searchTerm",
+                element: <Questions />,
+            },
+            {
+                path: "/u/create",
+                element: <QuestionCreator />,
+            },
+            {
+                path: "/u/:id",
+                element: <QuestionDetails />,
+            },
+            {
+                path: "/u/account",
+                element: <Account />,
+            },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
