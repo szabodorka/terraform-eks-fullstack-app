a7836ca24c05fcdd85b0665e97aef341ff18d7bf
d0rk-a <szabodorkam@gmail.com>
2025-05-22 18:47:25 +0200

    added postDate to Question component

--- frontend/askmate-frontend/src/pages/Questions/Question.jsx
+++ frontend/askmate-frontend/src/pages/Questions/Question.jsx
@@ -1,13 +1,20 @@
 import {useNavigate} from 'react-router-dom';
 
-const Question = ({id, title, description}) => {
+const Question = ({id, title, description, postDate}) => {
     const navigate = useNavigate();
 
     const handleClick = () => {
         navigate(`/u/${id}`);
     };
     return <>
-        <h2 onClick={() => handleClick(id)}>{title}</h2>
+        <div className="questionHeader">
+            <h2 onClick={() => handleClick(id)}>{title}</h2>
+            <p className="questionPostDate">{new Date(postDate).toLocaleString('en-UK', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            })}</p>
+        </div>
         <p>{description}</p>
     </>
 }
