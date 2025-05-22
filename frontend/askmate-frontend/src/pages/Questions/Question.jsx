92d8581f520327a802811bfcd995ef26b16fb26a
d0rk-a <szabodorkam@gmail.com>
2025-05-22 11:59:26 +0200

    corrected Question component according to previous commit in questions-page branch

--- frontend/askmate-frontend/src/pages/Questions/Question.jsx
+++ frontend/askmate-frontend/src/pages/Questions/Question.jsx
@@ -1,7 +1,15 @@
-const Question = ({title, description}) => {
+import {useNavigate} from 'react-router-dom';
+
+const Question = ({id, title, description}) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(`/u/${id}`);
+    };
     return <>
-        <h2>{title}</h2>
+        <h2 onClick={() => handleClick(id)}>{title}</h2>
         <p>{description}</p>
     </>
 }
+
 export default Question;
