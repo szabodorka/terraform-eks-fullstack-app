e63b5694211785341f1d298c3bb5efed87837254
d0rk-a <szabodorkam@gmail.com>
2025-05-22 10:45:05 +0200

    corrected delete endpoint and modified Question component rendering to transfer id

--- frontend/askmate-frontend/src/pages/Questions/Questions.jsx
+++ frontend/askmate-frontend/src/pages/Questions/Questions.jsx
@@ -1,17 +1,12 @@
 import { useEffect, useState } from "react";
 import Loading from "../../components/Loading/Loading.jsx";
 import Question from "./Question.jsx";
+import Error from "../../components/Error/Error.jsx";
 
-const fetchQuestions = async () => {
-    const response = await fetch("/api/question/all");
-    if (!response.ok) {
-        throw new Error(`Failed to fetch questions. ${response.statusText}`);
-    }
-    return response.json();
-};
+
 
 const deleteQuestion = async (id) => {
-    const response = await fetch(`/api/questions/${id}`, { method: "DELETE" });
+    const response = await fetch(`/api/question/${id}`, { method: "DELETE" });
     if (!response.ok) {
         throw new Error(`Failed to delete question. ${response.statusText}`);
     }
@@ -20,10 +15,23 @@ const deleteQuestion = async (id) => {
 const Questions = () => {
     const [loading, setLoading] = useState(true);
     const [questions, setQuestions] = useState(null);
+    const [error, setError] = useState(null);
+
+    const fetchQuestions = async () => {
+        const response = await fetch("/api/question/all");
+        if (!response.ok) {
+            if (response.status === 404) {
+                setError("Questions Not Found");
+            } else if (response.status === 500) {
+                setError("Internal Server Error")
+            }
+        }
+        return response.json();
+    };
 
     const handleDelete = (id) => {
         deleteQuestion(id).then(() => {
-            setQuestions((prev) => prev.filter((q) => q.id !== id));
+            setQuestions((prev) => prev.filter((question) => question.id !== id));
         });
     };
 
@@ -39,9 +47,11 @@ const Questions = () => {
 
     return (
         <>
+            {error && <Error errorMessage={error} />}
             {questions.map((question) => (
                 <div key={question.id}>
                     <Question
+                        id={question.id}
                         title={question.title}
                         description={question.description}
                     />
