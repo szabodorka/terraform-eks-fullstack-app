d4e239f049d6b4ffacc340b12b26b582d269dead
d0rk-a <szabodorkam@gmail.com>
2025-05-22 18:36:01 +0200

    updated Answer component to accomodate css design

--- frontend/askmate-frontend/src/pages/Answers/Answer.jsx
+++ frontend/askmate-frontend/src/pages/Answers/Answer.jsx
@@ -1,10 +1,20 @@
 
 const Answer = ({id, title, message, postDate}) => {
-    return <div key={id}>
-        <h2>{title}</h2>
-        <p>{message}</p>
-        <p className="postDate">{postDate}</p>
-    </div>
+    return (
+        <div key={id}>
+            <div className="answerHeader">
+                <h2>{title}</h2>
+                <p className="postDate">{new Date(postDate).toLocaleString('en-UK', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit'
+                })}</p>
+            </div>
+            <p>{message}</p>
+        </div>
+    )
 }
 
 export default Answer;
