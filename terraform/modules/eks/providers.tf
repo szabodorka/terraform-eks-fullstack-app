c393d7919972b75898ee86f0c8a89952777b1277
szabodorka <szabodorkam@gmail.com>
2025-09-22 19:41:36 +0200

    chore(infra/eks): declare variables and outputs for EKS module

---
+++ terraform/modules/eks/providers.tf
@@ -0,0 +1,17 @@
+provider "kubernetes" {
+  host                   = aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
+
+  registry_config_path   = "${path.root}/.helm/registry.json"
+  repository_cache       = "${path.root}/.helm/cache"
+  repository_config_path = "${path.root}/.helm/repositories.yaml"
+}
